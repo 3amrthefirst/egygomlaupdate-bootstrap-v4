f3d85f00fada995019a94ffbbdcf974f02797c65
3amrthefirst <76980166+3amrthefirst@users.noreply.github.com>
2022-04-04 01:11:30 +0200

    replacement and return products page
    hi 5olou Ramdan kaream

--- js/script.js
+++ js/script.js
@@ -7,3 +7,96 @@ function decrement() {
   document.getElementById('demoInput').stepDown();
 }
 /* mm*/
+
+/* erplacement modal for the 2nd edition*/
+$(document).ready(function() {
+  prep_modal();
+});
+
+function prep_modal()
+{
+  $(".modal").each(function() {
+
+  var element = this;
+    var pages = $(this).find('.modal-split');
+
+  if (pages.length != 0)
+  {
+    	pages.hide();
+    	pages.eq(0).show();
+
+    	var b_button = document.createElement("button");
+                b_button.setAttribute("type","button");
+          			b_button.setAttribute("class","btn btn-dark");
+          			b_button.setAttribute("style","display: none;");
+          			b_button.innerHTML = "السابق";
+
+    	var n_button = document.createElement("button");
+                n_button.setAttribute("type","button");
+          			n_button.setAttribute("class","btn btn-dark ");
+          			n_button.innerHTML = "التالي";
+
+    	$(this).find('.modal-footer').append(b_button).append(n_button);
+
+
+    	var page_track = 0;
+
+    	$(n_button).click(function() {
+        
+        this.blur();
+
+    		if(page_track == 0)
+    		{
+    			$(b_button).show();
+    		}
+
+    		if(page_track == pages.length-2)
+    		{
+				$(n_button).text("استبدال");
+          		n_button.setAttribute("class","btn btn-success ");
+    		}
+
+        if(page_track == pages.length-1)
+        {
+          $(element).find("form").submit();
+        }
+
+    		if(page_track < pages.length-1)
+    		{
+    			page_track++;
+
+    			pages.hide();
+    			pages.eq(page_track).show();
+    		}
+
+
+    	});
+
+    	$(b_button).click(function() {
+
+    		if(page_track == 1)
+    		{
+    			$(b_button).hide();
+    		}
+
+    		if(page_track == pages.length-1)
+    		{
+				$(n_button).text("التالي")
+				n_button.setAttribute("class","btn btn-dark ");
+    		}
+
+    		if(page_track > 0)
+    		{
+    			page_track--;
+
+    			pages.hide();
+    			pages.eq(page_track).show();
+    		}
+
+
+    	});
+
+  }
+
+  });
+}
